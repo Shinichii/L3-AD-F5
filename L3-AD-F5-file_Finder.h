cc89667b697d9ee99eedf43501d2a70b072c3a86
Aurelien DENIS <sethyxakarnis@gmail.com>
2017-02-26 22:01:38 +0100

    Ajout des fichiers nous permettant de lister et recuperer les fichiers dans le dossier

---
+++ L3-AD-F5-file_Finder.h
@@ -0,0 +1,20 @@
+/*
+	@Auteur : Aurelien DENIS
+	@Matiere : Aide a la decision
+	@Groupe : F5
+*/
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+using namespace std;
+
+namespace fs = std::experimental::filesystem 
+
+bool extensionValide(string s, string extension);
+
+void fetchElements(vector<string>& listeFichiers, string extension);
+
+string choisirElement(string extension);
+
